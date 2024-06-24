ea5b2c53f099b43562dfb658671161f9aa32632d
Aymen-benyounes008 <141510034+Aymen-benyounes008@users.noreply.github.com>
2024-06-24 19:10:02 +0200

    second-commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: '/Tip-Calculator/',
+  base: '/Tip-Calculator/',
   server:{
     port:3000
   },
